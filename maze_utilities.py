fda89383ae860a52f59e7b73d5660b9989bfb50a
Gianluca Villani <gvillani@kth.se>
2018-11-22 16:28:07 +0100

    first commit

---
+++ maze_utilities.py
@@ -0,0 +1,249 @@
+import numpy as np
+import copy
+from abc import ABCMeta, abstractmethod
+import random
+
+def cprint(cstring):
+    cstring = '{0: <3}'.format(cstring)
+    print(cstring.format(), end="", flush=True)
+
+
+class MazeAgent:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, init_state, maze, possible_actions):
+        self.state = init_state
+        self.maze = maze
+        self.possible_actions = possible_actions
+
+    def update_state(self, action):
+        self.state.update(action)
+
+
+class MazePlayer(MazeAgent):
+
+    def choose_action(self, beast_state, time_step):
+        x_p = self.state.x
+        y_p = self.state.y
+        x_b = beast_state.x
+        y_b = beast_state.y
+        policy = self.policies[(x_p, y_p, x_b, y_b)]
+        return policy[-(time_step+1)][0]
+
+    def collect_reward(self):
+        pass
+
+    def init_policy(self):
+        pass
+
+    def update_policy(self):
+        pass
+
+    def init_policies(self):
+        # Build all the possible states
+        seq = []
+        for x_p in range(self.maze.length):
+            for y_p in range(self.maze.height):
+                for x_b in range(self.maze.length):
+                    for y_b in range(self.maze.height):
+                        seq.append((x_p, y_p, x_b, y_b))
+        self.policies = dict.fromkeys(seq)
+        self.maze_dim = self.maze.length * self.maze.height
+
+
+class MazeBeast(MazeAgent):
+    def choose_action(self):
+        return random.choice(self.possible_actions)
+
+
+class Maze:
+    """
+    (0,0) top left ->
+    """
+    def __init__(self, length, height, goal_state):
+        self.length = length
+        self.height = height
+        self.vert_walls = []
+        self.horiz_walls = []
+        self.outer_horiz_walls = []
+        self.outer_vert_walls = []
+
+        self.add_vert_wall(-1, 0, height-1, outer=True)
+        self.add_vert_wall(length-1, 0, height-1, outer=True)
+        self.add_horiz_wall(-1, 0, length-1, outer=True)
+        self.add_horiz_wall(height-1, 0, length-1, outer=True)
+
+        self.goal_state = goal_state
+
+    def add_vert_wall(self, x, y_min, y_max, outer=False):
+        self.vert_walls.append({'x': x, 'y_min': y_min, 'y_max': y_max})
+        if outer:
+            self.outer_vert_walls.append({'x': x, 'y_min': y_min, 'y_max': y_max})
+
+    def add_horiz_wall(self, y, x_min, x_max, outer = False):
+        self.horiz_walls.append({'y': y, 'x_min': x_min, 'x_max': x_max})
+        if outer:
+            self.outer_horiz_walls.append({'y': y, 'x_min': x_min, 'x_max': x_max})
+
+    def check_player_action(self, action, state):
+        if action == 'stay':
+            return True
+
+        if action == 'down':
+            for h_wall in self.horiz_walls:
+                if h_wall['x_min'] <= state.x <= h_wall['x_max'] and state.y == h_wall['y']:
+                    return False
+            return True
+
+        if action == 'up':
+            for h_wall in self.horiz_walls:
+                if h_wall['x_min'] <= state.x <= h_wall['x_max'] and state.y == h_wall['y']+1:
+                    return False
+            return True
+
+        if action == 'right':
+            for v_wall in self.vert_walls:
+                if v_wall['y_min'] <= state.y <= v_wall['y_max'] and state.x == v_wall['x']:
+                    return False
+            return True
+
+        if action == 'left':
+            for v_wall in self.vert_walls:
+                if v_wall['y_min'] <= state.y <= v_wall['y_max'] and state.x == v_wall['x']+1:
+                    return False
+            return True
+
+    def check_beast_action(self, action, state):
+        if action == 'stay':
+            return True
+
+        if action == 'down':
+            for h_wall in self.outer_horiz_walls:
+                if h_wall['x_min'] <= state.x <= h_wall['x_max'] and state.y == h_wall['y']:
+                    return False
+            return True
+
+        if action == 'up':
+            for h_wall in self.outer_horiz_walls:
+                if h_wall['x_min'] <= state.x <= h_wall['x_max'] and state.y == h_wall['y'] + 1:
+                    return False
+            return True
+
+        if action == 'right':
+            for v_wall in self.outer_vert_walls:
+                if v_wall['y_min'] <= state.y <= v_wall['y_max'] and state.x == v_wall['x']:
+                    return False
+            return True
+
+        if action == 'left':
+            for v_wall in self.outer_vert_walls:
+                if v_wall['y_min'] <= state.y <= v_wall['y_max'] and state.x == v_wall['x'] + 1:
+                    return False
+            return True
+
+    def compute_feasible_player_future_states(self, allowed_actions, player_state):
+        # TODO: adjust in the init
+        feasible_actions = []
+        for action in allowed_actions:
+            if self.check_player_action(action, player_state):
+                feasible_actions.append(action)
+        return feasible_actions
+
+    def compute_feasible_beast_future_states(self, allowed_actions, beast_state):
+        # TODO: adjust in the init
+        feasible_actions = []
+        for action in allowed_actions:
+            if self.check_beast_action(action, beast_state):
+                feasible_actions.append(action)
+        return feasible_actions
+
+
+class MazeState:
+    def __init__(self, x_0, y_0):
+        self.x = x_0
+        self.y = y_0
+
+    def update(self, action):
+        if action == 'stay':
+            pass
+
+        if action == 'down':
+            self.y += 1
+
+        if action == 'up':
+            self.y -= 1
+
+        if action == 'right':
+            self.x += 1
+
+        if action == 'left':
+            self.x -= 1
+
+class MazeSimulation():
+    def __init__(self, player_init_state, beast_init_state, maze):
+        self.player = MazePlayer(player_init_state, maze)
+        self.beast = MazeBeast(beast_init_state, maze)
+        self.maze = maze
+        self.simulation_time = 0
+
+    def get_player_action(self, action=None):
+        if action == None:
+            action = self.player.choose_action()
+        return action
+
+    def get_beast_action(self, action=None):
+        if action == None:
+            action = self.beast.choose_action()
+        return action
+
+    def step_simulation(self):
+        player_action = self.get_player_action()
+        beast_action = self.get_beast_action()
+        self.player.update_state(player_action)
+        self.beast.update_state(beast_action)
+        self.simulation_time +=1
+
+class MazeGame():
+    def __init__(self, player, beast, maze):
+        self.player = player
+        self.beast = beast
+        self.maze = maze
+        self.sim_time = 0
+
+    def step_simulation(self):
+
+        p_action = self.player.choose_action(time_step=self.sim_time, beast_state=self.beast.state)
+        beast_action_feasible = False
+        while not beast_action_feasible:
+            b_action = self.beast.choose_action()
+            beast_action_feasible=self.maze.check_beast_action(action=b_action, state=self.beast.state)
+        if self.player.state.x == self.beast.state.x and self.player.state.y == self.beast.state.y:
+            b_action = 'stay'
+            p_action = 'stay'
+
+        self.player.update_state(p_action)
+        self.beast.update_state(b_action)
+        self.sim_time +=1
+
+    def plot_game(self):
+        print('Map at step: '+str(self.sim_time))
+        x_p = self.player.state.x
+        y_p = self.player.state.y
+        x_b = self.beast.state.x
+        y_b = self.beast.state.y
+
+        for y in range(self.maze.height):
+            print()
+            for x in range(self.maze.length):
+                if x_p == x_b and y_p == y_b:
+                    if x_p == x and y_p == y:
+                        cprint('O')
+                else:
+                    if x_p == x and y_p == y:
+                        cprint('P')
+                    elif x_b == x and y_b == y:
+                        cprint('B')
+                    else:
+                        cprint(' ')
+                cprint('|')
+        print()
---
+++ optimal_control.py
@@ -0,0 +1,177 @@
+from maze_utilities import *
+import copy
+import operator
+import time
+import random
+
+DEBUG = False
+
+def log(s):
+    if DEBUG:
+        print(s)
+
+
+class BellmansOptimizer():
+
+    def __init__(self, player, maze, player_actions, beast_actions, T=15):
+        self.player = player
+        self.maze = maze
+        self.T = T
+        self.player_actions = player_actions
+        self.beast_actions = beast_actions
+        print('Bellman built')
+
+    def solve_bellman(self):
+        t = self.T
+        i = 0
+        while t >= 0:
+            log('Bellmans step: ' +str(t))
+            current_step = self.T - t -1
+            for x_p in range(self.player.maze.length):
+                for y_p in range(self.player.maze.height):
+                    for x_b in range(self.player.maze.length):
+                        for y_b in range(self.player.maze.height):
+                            if self.player.policies[(x_p, y_p, x_b, y_b)] == None:
+                                self.player.policies[(x_p, y_p, x_b, y_b)] = []
+                            log('* -------------------------------------------------------------------- *')
+                            log('State analyzed: '+str(x_p)+', '+str(y_p)+', '+str(x_b)+', '+str(y_b)+ ', step: '+str(t))
+                            dead_end = False
+                            player_state = MazeState(x_p, y_p)
+                            beast_state = MazeState(x_b, y_b)
+                            action = None
+
+                            if t == self.T:
+                                r_T = compute_reward(t, maze=self.maze, player_state=player_state, beast_state=beast_state, action=action, final=True)
+                                dead_end = True
+                                self.player.policies[(x_p, y_p, x_b, y_b)].append(('stay', r_T))
+                                log('     ' + 'u_T = ' + str(r_T))
+
+                            else:
+                                r_t = compute_reward(t, self.maze, player_state, beast_state, action, False)
+                                log('     ' + 'u_t = ' + str(r_t))
+
+                            if not dead_end:
+                                beast_feasible_actions = []
+                                beast_future_states = []
+                                for action in self.beast_actions:
+                                    if self.maze.check_beast_action(action, beast_state):
+                                        beast_feasible_actions.append(action)
+                                        new_beast_state = copy.deepcopy(beast_state)
+                                        new_beast_state.update(action)
+                                        beast_future_states.append(new_beast_state)
+
+                                log('Players feasible actions: ')
+                                player_feasible_actions = self.player_actions
+
+                                if player_state.x == self.maze.goal_state.x and player_state.y == self.maze.goal_state.y:
+                                    player_feasible_actions = ['stay']
+                                    beast_future_states = [copy.deepcopy(beast_state)]
+
+                                if player_state.x == beast_state.x and player_state.y == beast_state.y:
+                                    player_feasible_actions = ['stay']
+                                    beast_future_states = [copy.deepcopy(beast_state)]
+
+                                seq = player_actions
+                                possible_policies_rewards = dict.fromkeys(seq, -1000)
+                                for action in player_feasible_actions:
+                                    u_t = 0
+                                    if self.maze.check_player_action(action, player_state):
+                                        log(action)
+                                        for future_state_beast in beast_future_states:
+                                            future_state_player = copy.deepcopy(player_state)
+                                            future_state_player.update(action)
+                                            x_f_p = future_state_player.x
+                                            y_f_p = future_state_player.y
+                                            x_f_b = future_state_beast.x
+                                            y_f_b = future_state_beast.y
+                                            u_star_next = self.player.policies[(x_f_p, y_f_p, x_f_b, y_f_b)][current_step][1]
+                                            u_t += 1 / len(beast_future_states) * u_star_next
+                                        u_t += r_t
+                                        possible_policies_rewards[action] = u_t
+                                        log('     '+ 'u_t = ' + str(u_t))
+                                #optimal_policy = max(possible_policies_rewards)
+                                optimal_policy = max(possible_policies_rewards.items(), key=operator.itemgetter(1))[0]
+                                optimal_future_cost = possible_policies_rewards[optimal_policy]
+                                log('Policy chosen: ')
+                                self.player.policies[(x_p, y_p, x_b, y_b)].append((optimal_policy, optimal_future_cost))
+                                log(self.player.policies[(x_p, y_p, x_b, y_b)])
+            t -= 1
+
+
+
+
+
+
+
+
+def compute_reward(t, maze, beast_state, player_state, action, final):
+    if player_state.x == maze.goal_state.x and player_state.y == maze.goal_state.y:
+        if final:
+            return 100
+        else:
+            return 0
+    if player_state.x == beast_state.x and player_state.y == beast_state.y:
+        return 0
+    return 0
+
+
+
+
+
+if __name__ == '__main__':
+    goal_state = MazeState(4, 4)
+    our_maze = Maze(6, 5, goal_state)
+    our_maze.add_horiz_wall(3, 1, 4)
+    our_maze.add_horiz_wall(1, 4, 5)
+    our_maze.add_vert_wall(1, 0, 2)
+    our_maze.add_vert_wall(3, 1, 2)
+    our_maze.add_vert_wall(3, 4, 4)
+
+    init_p_state = MazeState(0, 0)
+    init_b_state = MazeState(4, 4)
+    player_actions = ['up', 'down', 'left', 'right', 'stay']
+    beast_actions = ['up', 'down', 'left', 'right', 'stay']
+
+    p1 = MazePlayer(init_p_state, our_maze, possible_actions=player_actions)
+    b1 = MazeBeast(init_b_state, our_maze, possible_actions=beast_actions)
+    p1.init_policies()
+
+
+    # Solve the optimal control problem
+
+    T = 15
+
+    b_opt = BellmansOptimizer(p1, our_maze, player_actions, beast_actions, T)
+    print(b_opt.maze.goal_state.x)
+    print(b_opt.maze.goal_state.y)
+    start = time.time()
+    b_opt.solve_bellman()
+    end = time.time()
+    print('COMPUTATION TIME: ')
+    print(end - start)
+
+    state = (0,0,4,4)
+    policies = p1.policies[state]
+
+    print('Optimal policy for the state: ', str(state))
+    for i in range(T+1):
+        print(policies[-(i+1)])
+    my_game = MazeGame(player=p1, beast=b1, maze=our_maze)
+    print("Player policies computed.")
+    print("Starting the game")
+    for j in range(10):
+        print('**********************************************')
+        print('final game: '+str(j))
+        del my_game
+        my_game = MazeGame(player=p1, beast=b1, maze=our_maze)
+        for i in range(T):
+            my_game.plot_game()
+            my_game.step_simulation()
+        my_game.plot_game()
+
+        my_game.plot_game()
+
+
+
+
+
